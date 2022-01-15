2fd425a187a20ef9378414afc78d744087971709
AzkaLuthfanR <azkaluthfanr@gmail.com>
2022-01-15 11:46:09 +0700

    menambahkan println pada file gitTest.java

--- gitTest.java
+++ gitTest.java
@@ -1,5 +1,6 @@
 public class gitTest{
     public static void main(String[] args) {
         System.out.println("membuat file yang sudah di commit ke github");
+        System.out.println("melakukan perubahan");
     }
 }
